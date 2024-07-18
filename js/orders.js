7e4958b496253971ccfcab403b275034f145a44e
sara temraz <saraelsayed750@gmail.com>
2024-07-18 18:45:11 +0300

    admin dash board html css js only without frameworks responsive night and dark theme

---
+++ js/orders.js
@@ -0,0 +1,32 @@
+[
+    {
+        "productName": "Ruko Fu Pro Drone",
+        "productNumber": "49347",
+        "paymentStatus": "Due",
+        "shipping": "Pending"
+    },
+    {
+        "productName": "Drone with Camera Drone",
+        "productNumber": "96996",
+        "paymentStatus": "Paid",
+        "shipping": "Delivered"
+    },
+    {
+        "productName": "GPS 4k Drone",
+        "productNumber": "22821",
+        "paymentStatus": "Paid",
+        "shipping": "Delivered"
+    },
+    {
+        "productName": "DJI Air 25",
+        "productNumber": "81475",
+        "paymentStatus": "Due",
+        "shipping": "Pending"
+    },
+    {
+        "productName": "Lozenge Drone",
+        "productNumber": "00482",
+        "paymentStatus": "Paid",
+        "shipping": "Delivered"
+    }
+]
